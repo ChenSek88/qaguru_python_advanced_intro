c96dfcb9554aac9f289cdeaf72884842b1306de3
ts <ts@slickjump.com>
2024-07-23 11:22:28 +0300

    First commit

---
+++ api_tests/test_user.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("..")
+from helpers.users import *
+
+
+def test_users():
+    get_user_by_id("1")
+    create_user("svetogor", "svetogor@gmail.com", 32)
+    update_user_by_id("1", "svyatogor")
+    delete_user_by_id("2")
