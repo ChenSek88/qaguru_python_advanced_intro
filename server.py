291ad6a65625cce89d7f5a3e2c6c63321eeaa0cc
ts <ts@slickjump.com>
2024-07-23 17:48:24 +0300

    small fixes

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -61,4 +60,4 @@ def create_user(name: str, email: str, age: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
