d6a795611158878006acf803d4645593496d31e5
ts <ts@slickjump.com>
2024-07-30 18:14:13 +0300

    pagination homework added and some fixed

--- models/User.py
+++ models/User.py
@@ -1,13 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, HttpUrl
 
 
 class User(BaseModel):
     id: int
-    email: str
-    name: str
+    email: EmailStr
+    first_name: str
+    last_name: str
+    avatar: HttpUrl
     archive: bool
 
 
 class UserCreate(BaseModel):
-    email: str
-    name: str
+    email: EmailStr
+    first_name: str
+    last_name: str
+    avatar: HttpUrl
+
+
+
+class PaginationResponse(BaseModel):
+    total: int
+    page: int
+    size: int
+    pages: int
+    items: list[User]
--- server.py
+++ server.py
@@ -1,27 +1,35 @@
 from fastapi import FastAPI, HTTPException
 from models.User import User, UserCreate
+from models.AppStatus import AppStatus
+from http import HTTPStatus
+import logging
+import json
+from fastapi_pagination import Page, add_pagination, paginate
 
 
 app = FastAPI()
+add_pagination(app)
 
-
-test_data = {
-    1: {"id": 1, "name": "Ivan", "email": "ivan@gmail.com", "archive": False},
-    2: {"id": 2, "name": "Alex", "email": "alex@gmail.com", "archive": False},
-    3: {"id": 3, "name": "Bob", "email": "bob@gmail.com", "archive": False},
-    4: {"id": 4, "name": "Alice", "email": "alice@gmail.com", "archive": False}
-}
+users: list[User] = []
 
 
 def user_get(user_id: int):
-    if user_id in test_data and not test_data[user_id].get("archive"):
-        return test_data[user_id]
-    raise HTTPException(status_code=404, detail="User not found")
+    non_arch_users = [user for user in users if not user.get("archive")]     
+    if user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail="Invalid user id")
+    if user_id > len(users):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+    return non_arch_users[user_id - 1]
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK)
 def root():
-    return {"message": "FastAPI microservice"}
+    return {"message": "FastAPI microservice is running"}
+
+
+@app.get("/status", status_code=HTTPStatus.OK)
+def status() -> AppStatus:
+    return AppStatus(users=bool(users))
 
 
 @app.get("/api/users/{user_id}")
@@ -30,15 +38,15 @@ def get_user_by_id(user_id: int):
     
 
 @app.get("/api/users")
-def get_non_archived_users(): 
-    users = [user for user in test_data.values() if not user.get("archive")] 
-    return users
-
+def get_non_archived_users() -> Page[User]: 
+    non_archived_users = [user for user in users if not user.get("archive")] 
+    return paginate(non_archived_users)
+    
 
 @app.put("/api/users/{user_id}")
 def change_user_name(user_id: int, new_name: str):
     user = user_get(user_id)
-    user["name"] = new_name
+    user["first_name"] = new_name
     return {"data": user}
 
 
@@ -51,10 +59,26 @@ def delete_user_by_id(user_id: int):
 
 @app.post("/api/users")
 def create_user(user: UserCreate):
-    new_id = len(test_data) + 1
-    new_user = {"id": new_id, "name": user.name, "email": user.email, "archive": False}
-    test_data[new_id] = new_user
+    new_id = len(users) + 1
+    new_user = {
+        "id": new_id, 
+        "first_name": user.first_name, 
+        "last_name": user.last_name, 
+        "email": user.email, 
+        "avatar": user.avatar,
+        "archive": False
+        }
+    logging.debug(users)
+    users.append(new_user)
     return {"data": new_user}
+
+
+with open("users.json") as f:
+    users = json.load(f)
+
+    
+for user in users:
+    User.model_validate(user)
 
 
 if __name__ == "__main__":
