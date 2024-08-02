3bcfc5ef9304f8ca9c6c9d83951d4878eb239486
ts <ts@slickjump.com>
2024-08-02 16:29:56 +0300

    some fixes

--- tests/test_user.py
+++ tests/test_user.py
@@ -16,37 +16,42 @@ def create_user(app_url):
 		"last_name": fake.last_name(),
 		"avatar": f"https://reqres.in/img/faces/{randint(1, 100)}-image.jpg",
 		"email": fake.free_email()}
-	response = requests.post(f"{app_url}/api/users", data=json.dumps(new_user))
-	assert response.status_code == HTTPStatus.CREATED
-	return response
+	user = requests.post(f"{app_url}/api/users", data=json.dumps(new_user))
+	assert user.status_code == HTTPStatus.CREATED
+	return user
 
 
-def test_create_user_and_validate_user_model(app_url):
+def test_create_user_and_validate_user_model_and_assert_in_get_request(app_url):
 	user = create_user(app_url)
 	assert user.status_code == HTTPStatus.CREATED
 	User.model_validate(user.json())
+	created_user = requests.get(f"{app_url}/api/users/{user.json()['id']}")
+	assert created_user.json() == user.json()
 
 
-def test_update_user_avatar_by_id_and_assert_new_avatar(app_url):
+def test_update_user_avatar_by_id_and_assert_new_avatar_in_get_request(app_url):
+	user = create_user(app_url)
 	new_avatar = f"https://reqres.in/img/faces/{randint(1, 100)}-image.jpg"
-	user_id = create_user(app_url).json()['id']
-	response = requests.patch(f"{app_url}/api/users/{user_id}", data = json.dumps({"avatar": new_avatar}))
-	assert response.status_code == HTTPStatus.OK
-	user = json.loads(response.text)
-	UserUpdate.model_validate(user)
-	assert user['avatar'] == new_avatar
+	user_for_update = requests.patch(f"{app_url}/api/users/{user.json()['id']}", data = json.dumps({"avatar": new_avatar}))
+	assert user_for_update.status_code == HTTPStatus.OK
+	UserUpdate.model_validate(user_for_update.json())
+	assert user_for_update.json()['avatar'] == new_avatar
+	updated_user = requests.get(f"{app_url}/api/users/{user_for_update.json()['id']}")
+	assert updated_user.json()['avatar'] == new_avatar
 
 
 def test_delete_user_by_id_and_get_user_not_found(app_url):
-	user_id = create_user(app_url).json()['id']
-	response = requests.delete(f"{app_url}/api/users/{user_id}")
-	assert response.status_code == HTTPStatus.OK
-	response = requests.get(f"{app_url}/api/users/{user_id}")
-	response.status_code == HTTPStatus.NOT_FOUND
+	user = create_user(app_url)
+	user_for_delete = requests.delete(f"{app_url}/api/users/{user.json()['id']}")
+	assert user_for_delete.status_code == HTTPStatus.OK
+	deleted_user = requests.get(f"{app_url}/api/users/{user.json()['id']}")
+	assert deleted_user.status_code == HTTPStatus.NOT_FOUND
+	assert deleted_user.json()["detail"] == "User not found"
+
 
 
 @pytest.mark.usefixtures("fill_test_data")
-def test_get_users(app_url):
+def test_get_users_and_validate_model(app_url):
     response = requests.get(f"{app_url}/api/users")
     assert response.status_code == HTTPStatus.OK
 
@@ -54,6 +59,3 @@ def test_get_users(app_url):
     for user in user_list:
         print(user)
         User.model_validate(user)
-
-
-
