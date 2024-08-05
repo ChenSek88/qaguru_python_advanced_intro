19e1cb1ff3469194116adde199058c34fe106653
ts <ts@slickjump.com>
2024-08-05 19:27:25 +0300

    fixes for run app in docker

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from contextlib import asynccontextmanager
+import logging
+
 import dotenv
 
 
@@ -11,12 +14,19 @@ from app.routers import status, users
 from app.database.engine import create_db_and_tables
 from fastapi_pagination import add_pagination
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    logging.warning("On startup")
+    create_db_and_tables()
+    yield
+    logging.warning("On shutdown")
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
 
 
 if __name__ == "__main__":
-    create_db_and_tables()
     uvicorn.run(app)
